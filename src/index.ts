f944cf46110ec67d6227480bff7ad23e590b9e6f
Efen Stakes <efenstakes101@gmail.com>
2022-06-03 11:04:43 +0300

    add basic scene and run it

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,37 @@
+import * as BL from 'babylonjs'
 
 
-function say(): void {
-    console.log("started")
-}
+// the canvas
+const canvasEL = document.getElementById("3d")
 
-say()
+console.log("canvasEL ", canvasEL)
+console.log("hola from ts ")
+
+// create engine
+const engine = new BL.Engine(canvasEL as HTMLCanvasElement)
+
+
+// create scene
+const scene = new BL.Scene(engine, {})
+
+
+
+// create light
+const light = new BL.HemisphericLight("Light", BL.Vector3.Zero(), scene)
+
+
+// create camera
+const camera = new BL.ArcRotateCamera("Camera", 1, 1, 20, BL.Vector3.Zero(), scene)
+camera.attachControl(true)
+
+
+// create cube
+const cube = BL.MeshBuilder.CreateBox(
+    "Box", 
+    { width: 2, height: 2, depth: 2 },
+    scene
+)
+
+
+
+engine.runRenderLoop(()=> scene.render())
